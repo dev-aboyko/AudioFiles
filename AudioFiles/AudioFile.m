df3eaa8c7ecbcbf7e514617d6fcecb83643ac52e
Alexey Boyko <dev.aboyko@gmail.com>
2014-11-19 12:34:33 +0700

    show generic metadata for file

--- AudioFiles/AudioFile.m
+++ AudioFiles/AudioFile.m
@@ -20,24 +20,8 @@
         self.location = location;
         self.title = @"generic title";
         self.artist = @"generic artist";
-        [self getMetadata];
     }
     return self;
-}
-
--(void)getMetadata{
-    AVURLAsset *asset = [AVURLAsset URLAssetWithURL:self.location options:nil];
-
-    NSArray *titles = [AVMetadataItem metadataItemsFromArray:asset.commonMetadata withKey:AVMetadataCommonKeyTitle keySpace:AVMetadataKeySpaceCommon];
-    NSArray *artists = [AVMetadataItem metadataItemsFromArray:asset.commonMetadata withKey:AVMetadataCommonKeyArtist keySpace:AVMetadataKeySpaceCommon];
-    NSLog(@"titles\n%@", titles);
-    NSLog(@"artists\n%@", artists);
-/*
-    AVMetadataItem *title = [titles objectAtIndex:0];
-    AVMetadataItem *artist = [artists objectAtIndex:0];
-    
-    self.title = title.stringValue;
-    self.artist = artist.stringValue;*/
 }
 
 @end
