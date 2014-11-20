604ee1559db2c044d898a1a104ce0b1f4bb8ffca
Alexey Boyko <dev.aboyko@gmail.com>
2014-11-20 10:27:43 +0700

    check file exists with real filename

--- AudioFiles/GDriveDownload.m
+++ AudioFiles/GDriveDownload.m
@@ -16,6 +16,7 @@
 @property (nonatomic, strong) NSURL* docDirectoryURL;
 @property (nonatomic, strong) NSURLSession* session;
 @property (nonatomic, strong) NSURLSessionDownloadTask* downloadTask;
+@property (nonatomic) BOOL checkFileExists;
 
 @end
 
@@ -29,16 +30,13 @@
         self.delegate = delegate;
         NSArray *URLs = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
         self.docDirectoryURL = [URLs objectAtIndex:0];
+        self.checkFileExists = YES;
     }
     return self;
 }
 
-- (void)startDownload{
-//    if (![self fileExists])
-        [self forceDownload];
-}
-        
-- (void)forceDownload{
+- (void)startDownload
+{
     NSString* string = [NSString stringWithFormat:@"https://drive.google.com/uc?export=download&confirm=no_antivirus&id=%@", self.downloadID];
     NSURL* URL = [NSURL URLWithString:string];
     NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
@@ -47,9 +45,35 @@
     self.downloadTask = [self.session downloadTaskWithURL:URL];
     [self.downloadTask resume];
 }
+        
+- (void)forceDownload
+{
+    self.checkFileExists = NO;
+    [self startDownload];
+}
 
--(void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location{
-    
+- (void)URLSession:(NSURLSession *)session
+               downloadTask:(NSURLSessionDownloadTask *)downloadTask
+               didWriteData:(int64_t)bytesWritten
+          totalBytesWritten:(int64_t)totalBytesWritten
+  totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
+    if (self.checkFileExists)
+    {
+        NSString* fileName = [[downloadTask response] suggestedFilename];
+        if ([self fileExists:fileName])
+        {
+            NSLog(@"file exists canceling download");
+            [downloadTask cancel];
+        }
+    }
+    self.checkFileExists = NO;
+}
+
+- (void)URLSession:(NSURLSession *)session
+      downloadTask:(NSURLSessionDownloadTask *)downloadTask
+    didFinishDownloadingToURL:(NSURL *)location
+{
     NSError *error;
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
@@ -67,9 +91,17 @@
         [self.delegate downloadError:self];
 }
 
-- (BOOL)fileExists{
+- (void)URLSession:(NSURLSession *)session
+              task:(NSURLSessionTask *)task
+didCompleteWithError:(NSError *)error
+{
+    if (error != nil && error.code == NSURLErrorCancelled)
+        [self.delegate downloadError:self];
+}
+
+- (BOOL)fileExists:(NSString*)fileName{
     NSFileManager* fileManager = [NSFileManager defaultManager];
-    NSURL *destinationURL = [self.docDirectoryURL URLByAppendingPathComponent:self.downloadID];
+    NSURL *destinationURL = [self.docDirectoryURL URLByAppendingPathComponent:fileName];
     [self.delegate downloadSuccess:destinationURL];
     return [fileManager fileExistsAtPath:[destinationURL path]];
 }
